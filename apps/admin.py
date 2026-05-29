3e813a130e3e0d59013d7bb821e478a9fec6269c
maqsudbek <maqsudbekyuldoshev053@gmail.com>
2026-05-29 20:26:21 +0500

    admin yozildi

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from apps.models import User
+
+
+@admin.register(User)
+class UserModelView(UserAdmin):
+    list_display = ("phone", "first_name", "last_name", "role", "is_staff")
+    search_fields = ("phone", "first_name", "last_name")
+    ordering = ("-created_at",)
+
+    fieldsets = (
+        (None, {"fields": ("phone", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "role", "avatar")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "created_at", "updated_at")}),
+    )
+    add_fieldsets = (
+        (None, {"fields": ("phone", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "role", "branch")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    readonly_fields = ("created_at", "updated_at", "last_login")
