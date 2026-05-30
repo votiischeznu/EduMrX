87a5db2fa42528d3fcf690a0673f6f8d97acc23f
votiischeznu <votiischeznu@gmail.com>
2026-05-30 11:15:31 +0500

    Delete frontend directory

--- frontend/src/components/ui/skeleton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { cn } from "Frontend/frontend/src/lib/utils";
-
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("skeleton h-4 w-full", className)} {...props} />;
-}
-
-export { Skeleton };
