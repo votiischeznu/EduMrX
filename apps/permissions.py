32133d346b819762544bf473bd278750b656a802
votiischezu <mukhar1cci@gmail.com>
2026-05-30 20:18:35 +0500

    CRUD

---
+++ apps/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+class IsSuperAdmin(BasePermission):
+    message = "Faqat Super Admin uchun ruxsat berilgan."
+
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_superuser
+        )
