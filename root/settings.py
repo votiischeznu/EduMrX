c65aefd757d2a6e5e67c39f0494fd01817a9d612
votiischezu <mukhar1cci@gmail.com>
2026-06-04 00:36:34 +0500

    media

--- root/settings.py
+++ root/settings.py
@@ -109,8 +109,21 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INSTALLED_APPS += ['storages']
+
+AWS_ACCESS_KEY_ID = os.getenv('SUPABASE_SERVICE_KEY')
+AWS_SECRET_ACCESS_KEY = os.getenv('SUPABASE_SERVICE_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('SUPABASE_BUCKET')
+AWS_S3_ENDPOINT_URL = f"{os.getenv('SUPABASE_URL')}/storage/v1/s3"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_ADDRESSING_STYLE = 'path'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f"{os.getenv('SUPABASE_URL')}/storage/v1/object/public/{os.getenv('SUPABASE_BUCKET')}/"
 
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
