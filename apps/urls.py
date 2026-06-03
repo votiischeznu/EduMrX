0d246d76bff9ca3a4424a0348641bbe6e32dd833
votiischezu <mukhar1cci@gmail.com>
2026-06-04 00:15:02 +0500

    settings.py

--- apps/urls.py
+++ apps/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
@@ -68,4 +70,4 @@ urlpatterns = [
         # ── ViewSet Router ────────────────────────────────────
         path('', include(api_router.urls)),
     ])),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- root/settings.py
+++ root/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
