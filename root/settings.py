439bee137f2b67614521d6e4ae8a0f083233ce60
votiischezu <mukhar1cci@gmail.com>
2026-06-06 12:15:06 +0500

    settings.py

--- root/settings.py
+++ root/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'apps.pagination.StudentPagination',
+    'DEFAULT_PAGINATION_CLASS': 'apps.pagination.CustomPagination',
     'PAGE_SIZE': 20,
 }
 
