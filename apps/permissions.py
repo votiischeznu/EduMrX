8ac48df83c299f01a9bc5d679ade71b7063ba0d9
votiischezu <mukhar1cci@gmail.com>
2026-06-23 17:15:28 +0500

    Manager + Branch corrected (todo done)

--- apps/permissions.py
+++ apps/permissions.py
@@ -5,39 +5,45 @@ class IsSuperAdmin(BasePermission):
     message = "Faqat tizim Super Admini uchun ruxsat berilgan."
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_superuser)
+        user = request.user
+        return bool(user and user.is_authenticated and user.is_superuser)
 
 
 class IsDirector(BasePermission):
     message = "Faqat Direktor uchun ruxsat berilgan."
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_director)
+        user = request.user
+        return bool(user and user.is_authenticated and user.is_director)
 
 
 class IsManager(BasePermission):
     message = "Faqat o'quv markazi Admini uchun ruxsat berilgan."
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_admin)
+        user = request.user
+        return bool(user and user.is_authenticated and user.is_admin)
 
 
 class IsTeacher(BasePermission):
     message = "Faqat O'qituvchi uchun ruxsat berilgan."
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_teacher)
+        user = request.user
+        return bool(user and user.is_authenticated and user.is_teacher)
 
 
 class IsParent(BasePermission):
     message = "Faqat Ota-ona uchun ruxsat berilgan."
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_parent)
+        user = request.user
+        return bool(user and user.is_authenticated and user.is_parent)
 
 
 class IsStudent(BasePermission):
     message = "Faqat O'quvchi uchun ruxsat berilgan."
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_student)
+        user = request.user
+        return bool(user and user.is_authenticated and user.is_student)
