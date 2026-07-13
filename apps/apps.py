5c917512d72d1226212b20a6987b69f2ab0fa8a0
mahsudbekyuldoshev <mahsudbekyuldoshev09@gmail.com>
2026-07-13 16:16:29 +0500

    changes

--- apps/apps.py
+++ apps/apps.py
@@ -5,8 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class AppsConfig(AppConfig):
     name = "apps"
     default_auto_field = "django.db.models.BigAutoField"
-
     verbose_name = _("O'quv Markazi Boshqaruvi")
 
-def ready(self):
-    import apps.signals.contact  # noqa
+    def ready(self):
+        import apps.signals.contact  # noqa
